2154d383da7c4878aae02eeafb9eb64973d03f36
Yao Hwang <yaohwang99@gmail.com>
2023-02-27 09:06:06 +0000

    Add gitignore

--- examples/servo_zeroing.py
+++ examples/servo_zeroing.py
@@ -7,10 +7,19 @@ from time import sleep
 pan = Servo(pin=13) # pan_servo_pin (BCM)
 tilt = Servo(pin=12) 
 
-while True:
-    pan.set_angle(0)
+for i in range(3):
+    pan.set_angle(10)
     tilt.set_angle(0)
     sleep(2)
+    pan.set_angle(-10)
+    tilt.set_angle(-90)
+    sleep(2)
 
+pan.set_angle(0)
+pan.set_angle(0)
+pan.set_angle(0)
+tilt.set_angle(0)
+tilt.set_angle(0)
+tilt.set_angle(0)
 
 
