335eb0e7a3d4e60024cfde72820ab55384641ac5
Yao Hwang <yaohwang99@gmail.com>
2023-02-27 11:00:28 +0000

    Add pid controller

--- project/stream.py
+++ project/stream.py
@@ -49,69 +49,92 @@ def move_servo():
             d = 1
         offset += d
         sleep(0.1)
-
-    
-
+def read_frame():
+    global vs, outputFrame, lock
+    frame = vs.read()
+    frame = cv2.rotate(frame, cv2.ROTATE_180)
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    lower1 = np.array([0, 100, 20])
+    upper1 = np.array([10, 255, 255])
+    lower2 = np.array([160,100,20])
+    upper2 = np.array([179,255,255])
+    lower_mask = cv2.inRange(hsv, lower1, upper1)
+    upper_mask = cv2.inRange(hsv, lower2, upper2)
+    mask = lower_mask + upper_mask
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+    rx = 300
+    ry = 300
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            rx = x
+            ry = y
+    # grab the current timestamp and draw it on the frame
+    timestamp = datetime.datetime.now(tz=pytz.utc)
+    timestamp = timestamp.astimezone(timezone('US/Pacific'))
+    cv2.putText(frame, timestamp.strftime(
+        "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    with lock:
+        outputFrame = frame.copy()
+    return 300 - rx, ry - 300
 def detect_motion():
     # grab global references to the video stream, output frame, and
     # lock variables
-    global vs, outputFrame, lock
-    # lower boundary RED color range values; Hue (0 - 10)
-    lower1 = np.array([0, 100, 20])
-    upper1 = np.array([10, 255, 255])
-    # upper boundary RED color range values; Hue (160 - 180)
-    lower2 = np.array([160,100,20])
-    upper2 = np.array([179,255,255])
-    # loop over frames from the video stream
+    # Define PID constants
+    # KP = 0.01
+    # KI = 0.01
+    # KD = 0.005
+    # Initialize variables
+    x_error_sum = 0
+    x_last_error = 0
+    y_error_sum = 0
+    y_last_error = 0
+    last_time = time.time()
     while True:
-        # read the next frame from the video stream, resize it,
-        # convert the frame to grayscale, and blur it
-        frame = vs.read()
-        frame = cv2.rotate(frame, cv2.ROTATE_180)
-        frame = imutils.resize(frame, width=600)
-        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-        # construct a mask for the color "green", then perform
-        # a series of dilations and erosions to remove any small
-        # blobs left in the mask
+        current_time = time.time()
+        x_error, y_error = read_frame()
+        time_diff = current_time - last_time
+        x_error_sum += x_error * time_diff
+        y_error_sum += y_error * time_diff
+        tilt_angle = tilt.get_angle() + controller(0.015, 0, 0.0005,y_error, y_last_error, y_error_sum, time_diff)
+        tilt.set_angle(tilt_angle)
+        pan_angle = pan.get_angle() + controller(0.015, 0, 0.0005,x_error, x_last_error, x_error_sum, time_diff)
+        pan.set_angle(pan_angle)
+        x_last_error = x_error
+        y_last_error = y_error
+        last_time = current_time
+        time.sleep(0.01)
+def controller(kp, ki, kd, error, last_error, error_sum, time_diff):
+    error_diff = error - last_error
+    derivative = error_diff / time_diff
+    error_sum += error * time_diff
+    integral = error_sum
+    output = kp * error + ki * integral + kd * derivative
+    return output
         
-        lower_mask = cv2.inRange(hsv, lower1, upper1)
-        upper_mask = cv2.inRange(hsv, lower2, upper2)
-        mask = lower_mask + upper_mask
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        # find contours in the mask and initialize the current
-        # (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        center = None
-        # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            # only proceed if the radius meets a minimum size
-            if radius > 10:
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
-                cv2.circle(frame, (int(x), int(y)), int(radius),
-                    (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
-        # grab the current timestamp and draw it on the frame
-        timestamp = datetime.datetime.now(tz=pytz.utc)
-        timestamp = timestamp.astimezone(timezone('US/Pacific'))
-        cv2.putText(frame, timestamp.strftime(
-            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-        # acquire the lock, set the output frame, and release the
-        # lock
-        with lock:
-            outputFrame = frame.copy()
 
 def generate():
 	# grab global references to the output frame and lock variables
@@ -156,9 +179,9 @@ if __name__ == '__main__':
     t = threading.Thread(target=detect_motion)
     t.daemon = True
     t.start()
-    servo_worker = threading.Thread(target=move_servo)
-    servo_worker.daemon = True
-    servo_worker.start()
+    # servo_worker = threading.Thread(target=move_servo)
+    # servo_worker.daemon = True
+    # servo_worker.start()
     # start the flask app
     app.run(host=args["ip"], port=args["port"], debug=True,
         threaded=True, use_reloader=False)
